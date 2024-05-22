2109895817381843f512b530a1bed8b3a0da464d
wuminye <huanlekf@163.com>
2024-05-22 17:43:33 +0200

    Update gen_n3d.py
    fix typos

--- gen_n3d.py
+++ gen_n3d.py
@@ -9,12 +9,12 @@ overlap = True
 
 
 # add scenes' names that need to be trained
-for name in ['sear_steak','cook_spinach']:
+for name in ['sear_steak','cook_spinach','coffee_martini']:
 
 
     filename = f'./run_{name}.sh'
     with open(filename,'w') as f:
-        f.write(f"python run_multiframe.py --config configs/n3d/{name}.py --frame_ids 0 --training_mode 0\n" )
+        f.write(f"python run_multiframe.py --config configs/N3D/{name}.py --frame_ids 0 --training_mode 0\n" )
 
         for i in range(0,200,step):
             if i!=0 and overlap:
@@ -22,7 +22,7 @@ for name in ['sear_steak','cook_spinach']:
             else:
                 tmp = " ".join([str(j) for j in range(i,i+step)])
 
-            f.write(f"python run_multiframe.py --config configs/n3d/{name}.py --frame_ids {tmp} --training_mode 1\n" )
+            f.write(f"python run_multiframe.py --config configs/N3D/{name}.py --frame_ids {tmp} --training_mode 1\n" )
 
     print(filename)
 
